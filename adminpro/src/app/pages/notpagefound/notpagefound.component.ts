fd22661f95d651c0f785d7ec275f2dab2ea80887
Chaparro10 <19170614@itculiacan.edu.mx>
2024-05-04 20:19:49 -0700

    page not found

--- adminpro/src/app/pages/notpagefound/notpagefound.component.ts
+++ adminpro/src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styles: ``
+  styleUrls: ['./notpagefound.component.html']
 })
 export class NotpagefoundComponent {
 
+
+  year=new Date().getFullYear();
 }
