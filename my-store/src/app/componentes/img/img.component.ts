e59f7ba6929af763d66fb8e1522d967922d3c169
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-09-24 18:21:52 -0700

    Comunicacion de componentes:Envio de datos con Inputs y Directiva IF else con template

--- my-store/src/app/componentes/img/img.component.ts
+++ my-store/src/app/componentes/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './img.component.html',
   styleUrl: './img.component.scss'
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit {
+
+  @Input() imagen: string = 'https://www.w3schools.com/howto/img_avatar.png';//recibir datos del padre con Input
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
 
 }
