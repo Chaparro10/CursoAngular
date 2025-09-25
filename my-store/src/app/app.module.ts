8848a59610a2b498227c096ee15a4d5c8322304e
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-09-24 18:06:28 -0700

    componente img

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './componentes/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent
   ],
   imports: [
     BrowserModule,
