58d7e313126ebe0e03375eacec99c504d3d11cbc
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-09-24 18:33:37 -0700

    comunicacion hijo padre
    A partir de la emisión de un evento, el decorador @Output() permite enviar mensajes desde un componente hijo hacia el padre.

--- my-store/src/app/componentes/img/img.component.ts
+++ my-store/src/app/componentes/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -8,12 +8,23 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
-  @Input() imagen: string = 'https://www.w3schools.com/howto/img_avatar.png';//recibir datos del padre con Input
+  @Input() imagen: string = '';//recibir datos del padre con Input
+  imageDefault='https://imgs.search.brave.com/My_Zsb7i_Fup2KsoPsM7kzNAq3TTa2BXD-vbED7OrLA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/NC8yMi8xNy8zNi93/b29kZW4tbi0xMzQ2/MTk3XzY0MC5wbmc';
+
+  @Output() loaded = new EventEmitter<string>();
 
   constructor(){}
 
   ngOnInit(): void {
       
+  }
+
+  imagenError(){
+    this.imagen=this.imageDefault
+  }
+
+  imagenLoaded(){
+   this.loaded.emit(`Evento del hijo ${this.imagen}`);
   }
 
 }
