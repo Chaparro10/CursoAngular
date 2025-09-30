43b49dd569fa138567b29102be8cd7208765c1da
Chaparro10 <19170614@itculiacan.edu.mx>
2025-09-29 23:26:15 -0700

    creacion de componentes en en angular
    ng new nombre-del-proyecto --no-standalone
    
    ng g c components/nombre-de-componente

---
+++ 01-fundamentos-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { PersonasComponent } from './components/personas/personas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    PersonasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 01-fundamentos-angular/src/app/components/persona/persona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  standalone: false,
+  templateUrl: './persona.component.html',
+  styleUrl: './persona.component.scss'
+})
+export class PersonaComponent {
+
+}
