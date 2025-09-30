95a0cab321674bfa332c2617a3207af5d90236e8
Chaparro10 <19170614@itculiacan.edu.mx>
2025-09-29 23:30:48 -0700

    generacion de componente inline(en linea)

--- 01-fundamentos-angular/src/app/components/personas/personas.component.ts
+++ 01-fundamentos-angular/src/app/components/personas/personas.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personas',
   standalone: false,
-  templateUrl: './personas.component.html',
+  template: '<h1>Listado de personas</h1> <app-persona></app-persona>',
   styleUrl: './personas.component.scss'
 })
 export class PersonasComponent {
