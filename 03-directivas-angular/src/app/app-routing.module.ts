0143346c423f0e2d3bfefb2f0305b684d5b4a3ff
Kevin Chaparro <kevinchaparro@Mac-Studio.local>
2025-09-30 18:41:24 -0700

    agregando routing

--- 03-directivas-angular/src/app/app-routing.module.ts
+++ 03-directivas-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonasComponent } from './components/personas/personas.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PersonasComponent},
+  {path:'personas',component:PersonasComponent},
+  {path:'personas/agregar',component:FormularioComponent},
+  {path:'personas/:id',component:FormularioComponent},
+  {path:'**',redirectTo:''} // Ruta por defecto para rutas no encontradas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
